7872dcaa3c8a443d6d9cc9efc6da8baf428fff39
Jyben <benjamin.letessier@gmail.com>
2021-05-13 18:08:15 +0200

    Add roles enum

---
+++ Data/Enums/Roles.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Twitchbot.Common.Models.Data.Enums
+{
+    public enum Roles
+    {
+        [Description("Admin")]
+        Admin,
+        [Description("Moderator")]
+        Moderator,
+        [Description("App")]
+        App,
+        [Description("User")]
+        User
+    }
+}
